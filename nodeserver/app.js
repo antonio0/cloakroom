42f1f221a13785dae8ad55f30802452dd7139ac1
Jessie <jessie.abramson@hotmail.fr>
2014-10-18 18:05:29 -0400

    Basic rgb color detection

--- nodeserver/app.js
+++ nodeserver/app.js
@@ -1,8 +1,14 @@
 var express = require('express')
+var detect = require('./color_detection.js');
 var app     = express()
 var path    = require('path');
+var bodyParser = require('body-parser');
+var formidable = require('formidable');
 
-app.use(express.static(path.join(__dirname, 'static')));
+
+//app.use( bodyParser.json() );
+//app.use( bodyParser.urlencoded() );
+//app.use(express.static(path.join(__dirname, 'static')));
 
 
 app.get('/', function (req, res) {
@@ -13,7 +19,17 @@ app.post('/clothes', function (req, res) {
   var color = req.param('color');
 })
 
-var server = app.listen(3001, function () {
+app.post('/color_detect', function(req, res) {
+  var form = new formidable.IncomingForm();
+  form.parse(req, function(err, field, files){})
+  form.on('end', function(fields, files) {
+    var fullname = this.openedFiles[0].path;
+    console.log(fullname);
+    detect.getColor(fullname, res);
+  });  
+})
+
+var server = app.listen(3002, function () {
 
   var host = server.address().address
   var port = server.address().port
