3492047c6600c2bdf68081373fe9978c952ac8d9
magicmark <mark@larah.me>
2014-10-18 17:08:03 +0100

    added static folder

--- nodeserver/app.js
+++ nodeserver/app.js
@@ -1,15 +1,23 @@
 var express = require('express')
-var app = express()
+var app     = express()
+var path    = require('path');
+
+app.use(express.static(path.join(__dirname, 'static')));
+
 
 app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
-var server = app.listen(3000, function () {
+
+
+
+var server = app.listen(3001, function () {
 
   var host = server.address().address
   var port = server.address().port
 
   console.log('Example app listening at http://%s:%s', host, port)
 
-})
+});
+
